8d18d8c5e644ec6f9af31f183dd9b695b73212f2
Brenon Ortega <brenon.ortega@hotmail.com>
2022-06-03 15:21:11 -0300

    Adds id return to ICommandRepository.Save

--- src/Awarean.Sdk.SharedKernel/Repositories/ICommandRepository.cs
+++ src/Awarean.Sdk.SharedKernel/Repositories/ICommandRepository.cs
@@ -3,7 +3,7 @@ namespace Awarean.Sdk.SharedKernel.Repositories;
 
 public interface ICommandRepository<T, V> where T : IEntity<V>
 {
-    Task SaveAsync(T entity);
+    Task<V> SaveAsync(T entity);
     Task UpdateAsync(V id, T updatedEntity);
     Task DeleteAsync(V id);
 }
